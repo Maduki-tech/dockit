7b36a5433aabbb0aa9bbe91322dd07130558a7d9
maduki-tech <d.schlueter1011@gmail.com>
2026-04-07 11:04:42 +0200

    feat: initial commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+    printWidth: 80,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    plugins: ['prettier-plugin-tailwindcss'],
 };
